e45d8f7fb68fd827571c94f924d7afe08bedd242
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-01 07:54:57 -0700

    Importing! Such an useful tool!

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,23 +1,2 @@
 #!/usr/bin/python3
-str = "The Zen of Python, by Tim Peters\n\n"
-str2 = "Beautiful is better than ugly.\n"
-str3 = "Explicit is better than implicit.\n"
-str4 = "Simple is better than complex.\n"
-str5 = "Complex is better than complicated\n"
-str6 = "Flat is better than nested.\n"
-str7 = "Sparse is better than dense.\n"
-str8 = "Readability counts.\n"
-str9 = "Special cases aren't special enough to break the rules.\n"
-str10 = "Although practicality beats purity.\n"
-str11 = "Errors should never pass silently.\n"
-str12 = "Unless explicitly silenced.\n"
-str13 = "In the face of ambiguity, refuse the temptation to guess.\n"
-str14 = "There should be one-- and preferably only one --obvious way to do it.\n"
-str15 = "Although that way may not be obvious at first unless you're Dutch.\n"
-str16 = "Now is better than never.\n"
-str17 = "Although never is often better than *right* now.\n"
-str18 = "If the implementation is hard to explain, it's a bad idea.\n"
-str19 = "If the implementation is easy to explain, it may be a good idea.\n"
-str20 = "Namespaces are one honking great idea -- let's do more of those!"
-print(f"{str}{str2}{str3}{str4}{str5}{str6}{str7}{str8}{str9}{str10}{str11}{str12}\
-      {str13}{str14}{str15}{str16}{str17}{str18}{str19}{str20}")
+import Zen_of_Python
