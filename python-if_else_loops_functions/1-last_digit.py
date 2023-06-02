535eaa5174d3cec6ecba96049131c193efee268c
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-02 14:28:18 -0700

    Start of the script, yet there is an issue with the output. It seems that sometimes it displays a number and reads it value incorrectly, such as: "Last digit of 5328 is 0\ and is greater than 5"

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is {number % 10}", end=" ")
+if number == 0:
+    print("and is 0")
 if number > 5:
     print("and is greater than 5")
-elif number == 0:
-    print("and is 0")
-elif number != 0 and number < 6:
+if number < 6 and number != 0:
     print("and is less than 6 and not 0")
