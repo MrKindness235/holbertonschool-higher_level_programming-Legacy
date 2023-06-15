a7475d631372fad40af8c19b668e6eaa88ae1698
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-15 10:01:42 -0700

    Square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,6 +14,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    """Returns area of square"""
 
     def area(self):
         size = self.__size
