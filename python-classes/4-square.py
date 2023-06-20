30fd2adfe1784dcf2d094ba7fb0d3a37d4d81d77
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-20 09:39:48 -0700

    The worst of pain Fear it is the name

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 class Square:
-    def size(self):
-        return (self.value)
-
-    def size(self, value):
-        self.value = value
 
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an interger")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an interger")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.value = value
 
     def area(self):
         size = self.__size
