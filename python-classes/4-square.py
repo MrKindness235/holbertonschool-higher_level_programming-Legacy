a7b311b71d23d47ae6df44c8049fa0c4a33fc9c6
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-23 06:09:57 -0700

    Square fixed. Needed comments.

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
+"""
+Now, the object can be modified by the world of data sorrounding it
+garnting it a philosphical sense of flow, which is to say,
+pull, push and touch.
+"""
+
+
 class Square:
+    """This, is a square"""
 
     def __init__(self, size=0):
         self.__size = size
@@ -7,6 +15,7 @@ class Square:
     @property
     def size(self):
         return (self.__size)
+    """This is a method that calls forth the Square's main property, it's size."""
 
     @size.setter
     def size(self, value):
@@ -15,7 +24,12 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.size = value
+    """A setter, made to recall the data property and to be affixed to the square"""
 
     def area(self):
         size = self.__size
         return (size * size)
+    """
+    With this las method, one can make the Square calculate itself,
+    giving it a sense of concioussness
+    """
