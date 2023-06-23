278995bb08d4c6fc98ceaceed1660d11078e4d7e
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-23 10:43:24 -0700

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function returns if a object is equal to a class"""
+
+
+def is_same_class(obj, a_class):
+    """This should do the trick"""
+
+    return (obj == a_class)
