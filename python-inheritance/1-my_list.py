fd9a6fb129cd42ec8385b03d4fb35f0c1a534fba
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-23 09:51:00 -0700

    This test thing is going to be a bit irritating...

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Today I learnt that life is not a thing
+but a state of the soul
+and that state only ends
+when we leave this world
+"""
+
+
+class MyList(list):
+
+    def print_sorted(self):
+        print(sorted(self))
+    """This method prints the contents of MyList"""
