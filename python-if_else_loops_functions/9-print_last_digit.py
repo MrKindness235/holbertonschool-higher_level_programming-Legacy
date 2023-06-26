d51d624d3504efd6ce74e744f416dea11c04ea3a
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-26 10:10:59 -0700

    THANK YOU SOL

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,12 @@ def print_last_digit(number):
     if number > 0:
         last_digit = number % 10
         print("{}".format(last_digit), end="")
+        return (last_digit)
     elif number == 0:
+        last_digit = number % 10
         print("0", end="")
+        return (last_digit)
     else:
-        last_digit = ((number * -1) % 10) * -1
-        print("{}".fomat(last_digit))
+        last_digit = ((number * -1) % 10)
+        print("{}".format(last_digit), end="")
+        return (last_digit)
