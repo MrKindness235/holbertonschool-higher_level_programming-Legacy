9c985cddcbb2d16b227bd903e4932e60c2cb8a2d
MrKindness235 <patriciocoitino235@gmail.com>
2023-06-30 08:33:38 -0700

    This might be the way to handle correctly str.form

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 def print_list_integer(my_list=[]):
     i = 0
     for i in range(0, len(my_list)):
-        print("{}".format(my_list[i]))
+        txt = "{}"
+        print(txt.format(my_list[i]))
