e3af79438166a88cecb737423381de424df61a35
MrKindness235 <patriciocoitino235@gmail.com>
2023-07-09 13:03:55 -0700

    Premiliminar changes

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+    listA = []
+    listB = []
+    for i in range(lenA):
+        listA.append(0)
+    for i in range(lenB):
+        listB.append(0)
+    print("listA: ", listA)
+    print("listB: ", listB)
