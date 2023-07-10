f00a7aed4cdd6bb09ce9b3716d69aee6506ea735
MrKindness235 <patriciocoitino235@gmail.com>
2023-07-10 06:08:36 -0700

    Tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,9 +4,28 @@ def add_tuple(tuple_a=(), tuple_b=()):
     lenB = len(tuple_b)
     listA = []
     listB = []
-    for i in range(lenA):
-        listA.append(0)
-    for i in range(lenB):
-        listB.append(0)
+    for i in range(2):
+        if not tuple_a[i]:
+            listA.append(tuple_a[i])
+        else:
+            listA.append(0)
+    for i in range(2):
+        if not tuple_a[i]:
+            listB.append(tuple_b[i])
+        else:
+            listB.append(0)
     print("listA: ", listA)
     print("listB: ", listB)
+
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    cpa = [0, 0, 0, 0]
+    cpb = [0, 0, 0, 0]
+
+    for i in range(len(tuple_a)):
+        cpa[i] = tuple_a[i]
+    for j in range(len(tuple_b)):
+        cpb[j] = tuple_b[j]
+
+    return(cpa[0] + cpb[0], cpa[1] + cpb[1])
