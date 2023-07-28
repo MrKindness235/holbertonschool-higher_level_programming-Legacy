96ea65dad2946e366c8847c769e2b6b95aabaef8
MrKindness235 <patriciocoitino235@gmail.com>
2023-07-28 06:55:14 -0700

    Is it indentation?

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,3 @@
 -- This one creates a table named second_table in the database hbtn_0c_0 in the MySQL server and adds multiple rows!
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT,	name VARCHAR(256),	score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, Jhon, 10), (2, Alex, 3), (3, Bob, 14), (4, George, 8);
