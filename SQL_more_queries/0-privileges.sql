9c6a7bb682fca64737cef5285b9eed1a2a35c332
MrKindness235 <patriciocoitino235@gmail.com>
2023-07-31 07:54:01 -0700

    Doubles!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- This pretty neaty itsy bitsy script lists all previleges
 -- of the MySQL uesrs user_0d_1 and user_0d_2 on server
-SHOW GRANTS FOR user_0d_1 AND user_0d_2;
+SHOW GRANTS FOR user_0d_1;
+SHOW GRANTS FOR user_0d_2;
